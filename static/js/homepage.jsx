10677a104c6de6ad011d19ca6c1e78688abd88b1
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-04-07 16:33:40 -0700

    first step add message, link and photo

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+        <div> 
+            <p>We're so happy you're here!</p>
+            <a href="/cards">Go to the cards page</a>
+            <img src="/static/img/balloonicorn.jpg" />
+        </div>
+        );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
