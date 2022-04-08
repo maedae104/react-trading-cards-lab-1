14196f9eea0b1625767c33885e35dd808129e6b9
Mae Dae <maedae@136-25-136-235.cab.webpass.net>
2022-04-07 17:37:41 -0700

    completed final step

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -2,13 +2,57 @@
 
 const tradingCardData = [
   {
-    name: 'Balloonicorn',
-    skill: 'video games',
-    imgUrl: '/static/img/balloonicorn.jpg',
+    name: "Balloonicorn",
+    skill: "video games",
+    imgUrl: "/static/img/balloonicorn.jpg",
+    cardId: 1,
   },
-
+  {
+    name: "Float",
+    skill: "baking pretzels",
+    imgUrl: "/static/img/float.jpg",
+    cardId: 2,
+  },
+  {
+    name: "Llambda",
+    skill: "knitting scarves",
+    imgUrl: "/static/img/llambda.jpg",
+    cardId: 3,
+  },
+  {
+    name: "Off-By-One",
+    skill: "climbing mountains",
+    imgUrl: "/static/img/off-by-one.jpeg",
+    cardId: 4,
+  },
+  {
+    name: "Seed.py",
+    skill: "making curry dishes",
+    imgUrl: "/static/img/seedpy.jpeg",
+    cardId: 5,
+  },
+  {
+    name: "Polymorphism",
+    skill: "costumes",
+    imgUrl: "/static/img/polymorphism.jpeg",
+    cardId: 6,
+  },
+  {
+    name: "Short Stack Overflow",
+    skill: "ocean animal trivia",
+    imgUrl: "/static/img/shortstack-overflow.jpeg",
+    cardId: 7,
+  },
+  {
+    name: "Merge",
+    skill: "bullet journaling",
+    imgUrl: "/static/img/merge.png",
+    cardId: 8,
+  },
 ];
 
+// define TradingCard component
+   // render one trading card
 function TradingCard(props) {
   return (
     <div className="card">
@@ -19,27 +63,25 @@ function TradingCard(props) {
   );
 }
 
-ReactDOM.render(
-  <TradingCard name="Balloonicorn" skill="video games" imgUrl="/static/img/balloonicorn.jpg" />,
-  document.querySelector('#balloonicorn'),
-);
 
-ReactDOM.render(
-  <TradingCard name="Float" skill="baking pretzels" imgUrl="/static/img/float.jpg" />,
-  document.querySelector('#float'),
-);
 
-ReactDOM.render(
-  <TradingCard name="Llambda" skill="knitting scarves" imgUrl="/static/img/llambda.jpg" />,
-  document.querySelector('#llambda'),
-);
+// define TradingCardContainer component
+function TradingCardContainer(props) {
+  const tradingCards = [];
 
-ReactDOM.render( 
-  <TradingCard name="merge" skill="chewing bubble gum" imgUrl="/static/img/merge.png" />,
-  document.querySelector('#merge'),
-);
+  for (const data of tradingCardData) {
+    tradingCards.push(
+      <TradingCard
+        name={data.name}
+        skill={data.skill}
+        imgUrl={data.imgUrl}
+        key={data.cardId}
+      />,
+    );
 
-ReactDOM.render( 
-  <TradingCard name="shortstack-overflow" skill="jumping jacks" imgUrl="/static/img/shortstack-overflow.jpeg" />,
-  document.querySelector('#shortstack'),
-);
+  }
+
+    return <React.Fragment>{tradingCards}</React.Fragment>; 
+    
+  }
+  ReactDOM.render(<TradingCardContainer />, document.querySelector('#all-cards'));
